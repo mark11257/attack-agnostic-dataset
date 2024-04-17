810725557c73ffd17426c5840a5852963d81461a
Mark Rafat <120990809+mark11257@users.noreply.github.com>
2024-04-17 12:24:22 +0200

    Update trainer.py

--- dfadetect/trainer.py
+++ dfadetect/trainer.py
@@ -298,7 +298,7 @@ class NNDataSetting:
 
 class Trainer():
     def __init__(self,
-                 epochs: int = 1,
+                 epochs: int = 20,
                  batch_size: int = 32,
                  device: str = "cpu",
                  optimizer_fn: Callable = torch.optim.Adam,
